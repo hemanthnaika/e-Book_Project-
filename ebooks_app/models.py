292953365416fc567a0c19597f6980dac4c4a1bc
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2024-11-21 09:53:09 +0530

    Day-2

--- ebooks_app/models.py
+++ ebooks_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title=models.CharField(max_length=200)
+    author=models.CharField(max_length=20)
+    description=models.TextField()
+    price=models.IntegerField()
+    
+
+   
